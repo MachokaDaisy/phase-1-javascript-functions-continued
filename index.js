b1d6063425deabf2c3468116e4ef00c9c0f47324
Machoka Daisy <machokadaisy25@gmail.com>
2022-06-08 14:32:19 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return "This Saturday, I want to " + activity + "!";
+}
+
+const mondayWork = function(item = "go to the office"){
+    return "This Monday, I will " + item +".";
+}
+
+function wrapAdjective(flair = "*"){
+    return function(prompt = "special"){
+        return `You are ${flair}${prompt}${flair}!`;
+    }
+}
